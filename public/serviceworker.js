78c8b2298fe96a784559d5cadb84f99dedcfcd43
jayson3110 <jayson7498@gmail.com>
2021-08-07 09:25:48 +0700

    ready for production

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -2,8 +2,8 @@ const CACHE_NAME = "version-1";
 const urlsToCache = ['index.html', 'offline.html'];
 
 const self = this;
-// Install SW
 
+// Install SW
 self.addEventListener('install', (event) => {
 	event.waitUntil(
 		caches.open(CACHE_NAME).then((cache) =>{
@@ -17,11 +17,26 @@ self.addEventListener('install', (event) => {
 
 
 // Listen for request
-self.addEventListner('fetch', (event) => {
+self.addEventListener('fetch', (event) => {
+	event.respondWith(caches.match(event.request).then(() => {
+		return fetch(event.request)
+		     .catch(() => caches.match('offline.html'))
+	}))
 	
 });
 
 // Activate the SW
-self.addEventListner('activate', (event) => {
-	
+self.addEventListener('activate', (event) => {
+	const cacheWhiteList = [];
+	cacheWhiteList.push(CACHE_NAME);
+
+	event.waitUntil(
+		caches.keys().then((cacheNames) => Promise.all(
+			cacheNames.map((cacheName) => {
+				if(!cacheWhiteList.includes(cacheName)) {
+					return caches.delete(cacheName);
+				}
+			})
+		))
+	)
 });
